a120e5129d188f5138f6814656bdcba1e340630e
Marilyn224 <maryobaikhedo@gmail.com>
2022-09-23 12:28:36 -0700

    Reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,4 @@
-#ifindef MAIN_H
-#define MAIN_H
-
-/**
-* void prototype ()
-* int prototype ()
-*/
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,6 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
-
-#endif /*MAIN_H*/
