bc4658d5dade8db8cf99b88c5f3341eb8b270e94
Marilyn224 <maryobaikhedo@gmail.com>
2022-09-23 10:33:08 -0700

    Reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 
 /**
  * reverse_array - reverses an array of integers
- * a: array to be reversed
+ * @a: array to be reversed
  * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
