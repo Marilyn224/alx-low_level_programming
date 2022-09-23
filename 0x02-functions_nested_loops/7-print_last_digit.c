d2bd6704ccbfdd3124a8b79503b3f4825611fd05
Marilyn224 <maryobaikhedo@gmail.com>
2022-09-23 15:31:19 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,19 +10,15 @@
 int print_last_digit(int n)
 {
 	int a;
+	
+	if (n < 0)
+		n = -n;
 
-	if (n > 10)
-	{
-		a = n % 10;
-	}
-	else
-	{
-		if (n < 10 && n >= 0)
-		{
-			_putchar(n + '0');
-			return (n);
-		}
-	}
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
 	_putchar(a + '0');
 	return (a);
 }
